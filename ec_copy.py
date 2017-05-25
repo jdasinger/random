249c0a232b57e4812edf27b1ac34e73717987483
jdasinger <james.dasinger@slate.com>
2017-05-25 18:19:01 -0400

    switched some hardcoded params to command line input variables

--- ec_copy.py
+++ ec_copy.py
@@ -2,17 +2,18 @@ import boto3
 
 region = 'us-east-1'
 
+cluster_id = sys.argv[1]
+target_bucket = sys.argv[2]
 
-# need to convert cache_cluster_id and target bucket to command line input variable
 
 def lambda_handler(event, context):
     try:
         source = boto3.client('elasticache', region_name=region)
         snapid = \
-            source.describe_snapshots(CacheClusterId='<cache_cluster_id>', SnapshotSource='system')['Snapshots'][-1][
+            source.describe_snapshots(CacheClusterId=cluster_id, SnapshotSource='system')['Snapshots'][-1][
                 'SnapshotName']
         source.copy_snapshot(SourceSnapshotName=snapid, TargetSnapshotName=snapid,
-                             TargetBucket='<target_bucket>')
+                             TargetBucket=target_bucket)
 
     except Exception as e:
         raise e
